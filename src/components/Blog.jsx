c465178f802a1736cb09a5b7adb17e8986ca845b
Can Kolho <can.kolho@hotmail.com>
2023-09-29 13:29:10 +0300

    5.8*: blogilistan frontend, step8

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [blogVisible, setBlogVisible] = useState(false)
+
+  const hideWhenVisible = { display: blogVisible ? 'none' : '' }
+  const showWhenVisible = { display: blogVisible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setBlogVisible(!blogVisible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}> 
+        {blog.title} {blog.author} {}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.title} {blog.author} {}
+        <button onClick={toggleVisibility}>hide</button>
+        <p>{blog.url}</p>
+        <p>
+          Likes {blog.likes}
+          <button onClick={() => console.log('liked')}>Like</button>
+        </p>
+        <p>{blog.user.name}</p>
+      </div>
+    </div>
+)}
 
 export default Blog
